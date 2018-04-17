18922aeb5ad8b007c8f934951e589b7bccd34059
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-17 18:15:17 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,7 @@
 class Student < ActiveRecord::Base
 
   def to_s
-    first_name = self.first_name
-    last_name = self.last_name
-    full_name = prepend(first_name, " ", last_name)
-    full_name
+    first_name << last_name
   end
 
 end
